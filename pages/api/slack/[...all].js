d94924aac1d9e36fa166e7bc121d57f9d4b5c134
eita-tachino <34756983+eita-tachino@users.noreply.github.com>
2022-07-26 10:40:43 +0900

    slack set ups

--- pages/api/slack/[...all].js
+++ pages/api/slack/[...all].js
@@ -1,7 +1,7 @@
 import { HTTPReceiver, LogLevel } from "@slack/bolt";
 
 const receiver = new HTTPReceiver({
-  signingSecret: "",
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
   processBeforeResponse: true,
   endpoints: "/api/slack/events",
   logLevel: LogLevel.DEBUG,
