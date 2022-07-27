6a890bbac313632710b61537a674ff91bc95f4e8
eita-tachino <34756983+eita-tachino@users.noreply.github.com>
2022-07-27 13:26:05 +0900

    base functionality is done

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import { NextRequest } from "next/server";
+
 export default function Home() {
   return (
     <div className={styles.container}>
